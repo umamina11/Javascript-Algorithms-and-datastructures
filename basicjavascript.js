5cc058138df6634417b50f846f5b6c78f638cd3a
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-03-07 16:50:00 +0530

    Create basicjavascript.js

---
+++ basicjavascript.js
@@ -0,0 +1,52 @@
+//I
+
+// the two diffrent style of comments in javascript 
+//1. is the double slash for single line 
+/**2 
+ * another one os back slash with double asteric sign 
+ * where we can comment multiple lines a time 
+ */
+
+//II
+//defiening variables with "var key word"
+var myName;
+var number;
+var person1;
+var myNameIs;
+
+//III
+//sorting values with the assignment operator "="
+
+var a;
+a= 7;
+
+
+
+//IV
+//assigning the value of one variable to another
+
+var p;
+p=90;
+var q;
+q=p;
+
+
+var name;
+name="uma";
+var fullName;
+fullName=name;
+
+//V
+//initializing variables with the assignment operator
+
+var b=4;
+var name1="umamina";
+
+//VI
+// declaring the string variables
+
+var myFirstName="Uma ";
+var myLastName="Mina";
+
+
+//VII
