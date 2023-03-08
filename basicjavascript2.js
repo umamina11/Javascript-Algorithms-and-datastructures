e2501e4638786fbab9bca36048ff614df53149c8
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-03-08 21:46:32 +0530

    Create basicjavascript2.js

---
+++ basicjavascript2.js
@@ -0,0 +1,46 @@
+//I
+//compound assignment with augmented addition 
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+a += 12;
+b += 9;
+c += 7;
+
+//II
+//compound assignement with augmented subtraction
+
+let p = 11;
+let q = 9;
+let r = 3;
+
+p -= 6;
+q -= 15;
+r -= 1;
+
+//III 
+// compound assignment with augmented multiplication
+let l = 5;
+let m = 12;
+let n = 4.6;
+
+l *= 5;
+m *= 3;
+n *= 10;
+
+//IV
+// compound assignment with augmented with division
+let e = 48;
+let f = 108;
+let g = 33;
+
+// Only change code below this line
+e /= 12;
+f /= 4;
+g /= 11;
+
+//V
+//escaping literal quotes in string
+const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
