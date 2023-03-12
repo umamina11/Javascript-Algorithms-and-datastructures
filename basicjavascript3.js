1a3cbde28f5e0a2c8cf8395d055d4f4cb7977856
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-03-12 19:38:15 +0530

    local scope and functions

--- basicjavascript3.js
+++ basicjavascript3.js
@@ -155,3 +155,23 @@ function fun2() {
   }
   console.log(output);
 }
+
+//local scope and the functions 
+/**The editor has two console.logs to help you see what is happening. Check the console as you code to see how it changes. Declare a local variable myVar inside myLocalScope and run the tests.
+
+Note: The console will still display ReferenceError: myVar is not defined, but this will not cause the tests to fail.
+
+ */
+
+function myLocalScope() {
+
+
+  console.log('inside myLocalScope', myVar);
+  var myVar = "foo";
+  
+  console.log(myVar);
+}
+
+myLocalScope();
+
+console.log('outside myLocalScope', myVar);
