e93e0bf49860122d6ab1053f630243a048c6b7cc
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-03-12 22:28:57 +0530

    comparison operator

--- basicjavascript4.js
+++ basicjavascript4.js
@@ -24,3 +24,41 @@ function testStrict(val) {
   }
   
   testStrict(10);
+
+  //III
+  // practice cmomparing differnt values
+  /**The compareEquality function in the editor compares two values using the equality operator. Modify the function so that it returns the string Equal only when the values are strictly equal. */
+
+function compareEquality(a, b) {
+    if (a === b) { 
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  compareEquality(10, "10");
+
+  //IV
+  // comparity with the inequality operator
+
+  function testNotEqual(val) {
+    if (val !=99) { 
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+  
+  testNotEqual(10);
+
+  // V
+  // comparision with the strict inequality operator
+ /* Add the strict inequality operator to the if statement so the function will return the string Not Equal when val is not strictly equal to 17
+ */
+ function testStrictNotEqual(val) {
+    if (val!==17) { 
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+  
+  testStrictNotEqual(10);
