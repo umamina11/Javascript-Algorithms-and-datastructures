ac54489038db633bec5eeb3e84594fd16f8b3a4d
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-03-12 21:43:55 +0530

    assigned with return value

--- basicjavascript3.js
+++ basicjavascript3.js
@@ -209,3 +209,15 @@ function addFive(){
 
 addThree();
 addFive();
+
+//XVII
+// Assigned with a returned value
+/**Call the processArg function with an argument of 7 and assign its return value to the variable processed. */
+
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+processed=processArg(7);
