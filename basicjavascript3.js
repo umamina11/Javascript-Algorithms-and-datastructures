c553175f3430e5ce73fffcfdb45392fca66baad7
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-03-12 21:53:32 +0530

    stand in line (push and shift )

--- basicjavascript3.js
+++ basicjavascript3.js
@@ -221,3 +221,23 @@ function processArg(num) {
 }
 
 processed=processArg(7);
+
+//XVIII
+//Stand in line
+/** Write a function nextInLine which takes an array (arr) and a number (item) as arguments.
+
+Add the number to the end of the array, then remove the first element of the array.
+
+The nextInLine function should then return the element that was removed.*/
+
+function nextInLine(arr, item) {
+  
+  arr.push(item);
+return arr.shift();
+}
+
+let testArr = [1, 2, 3, 4, 5];
+
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
