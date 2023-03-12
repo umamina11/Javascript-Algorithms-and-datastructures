54cee14d1196d9764532b1df0ce9494612bdc8e8
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-03-12 21:34:21 +0530

    undefined value returned from a function

--- basicjavascript3.js
+++ basicjavascript3.js
@@ -193,3 +193,19 @@ const outerWear="sweater";
 myOutfit();
 
 //XVI
+// understanding undefined value returened from a funtion
+/**Create a function addFive without any arguments. This function adds 5 to the sum variable, but its returned value is undefined.
+ */
+
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+function addFive(){
+  sum += 5;
+}
+
+addThree();
+addFive();
