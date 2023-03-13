597313ee1c9f470f1bda9313a5643074e4d81173
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-03-13 10:24:13 +0530

    logical operators

--- basicjavascript4.js
+++ basicjavascript4.js
@@ -148,3 +148,19 @@ function compareEquality(a, b) {
     return "No";
   }
   testLogicalAnd(10);
+
+  //XI
+  // Comparison with the logical OR operator
+  //Combine the two if statements into one statement which returns the string Outside if val is not between 10 and 20, inclusive. Otherwise, return the string Inside.
+
+  function testLogicalOr(val) {
+    
+    if (val< 10 || val > 20) {
+      return "Outside";
+    }
+  
+    return "Inside";
+  }
+  
+  testLogicalOr(15);
+
