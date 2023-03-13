1c20d25989b56f0ae886a3e2c9a1947ca7b9321b
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-03-13 18:51:47 +0530

    Switch statements

---
+++ switchStatements.js
@@ -0,0 +1,96 @@
+//I
+//selecting from many options with switch statement 
+/**Write a switch statement which tests val and sets answer for the following conditions:
+1 - alpha
+2 - beta
+3 - gamma
+4 - delta
+
+ */
+function caseInSwitch(val) {
+    let answer = "";
+  switch (val){
+    case 1: 
+    return "alpha";
+    break;
+    case 2:
+    return "beta";
+    break;
+    case 3: 
+    return "gamma";
+    break;
+    case 4: 
+    return "delta";
+  }
+  
+    return answer;
+  }
+  
+  caseInSwitch(1);
+
+  //II
+  // Adding a default option in Switch statements
+  /**Write a switch statement to set answer for the following conditions:
+a - apple
+b - bird
+c - cat
+default - stuff */
+
+function switchOfStuff(val) {
+    let answer = "";
+    
+  switch(val){
+    case "a":
+    return "apple";
+    break;
+    case "b":
+    return "bird";
+    break;
+    case "c":
+    return "cat";
+    break;
+    default:
+    return "stuff";
+    break;
+  }
+  
+    return answer;
+  }
+  
+  switchOfStuff(1);
+
+//III
+//multiple identical options in switch statements
+
+/**Write a switch statement to set answer for the following ranges:
+1-3 - Low
+4-6 - Mid
+7-9 - High
+
+Note: You will need to have a case statement for each number in the range. */
+
+function sequentialSizes(val) {
+    let answer = "";
+  switch (val){
+    case 1:
+    case 2: 
+    case 3:
+    return "Low";
+    break;
+    case 4: 
+    case 5: 
+    case 6:
+    return "Mid";
+    case 7:
+    case 8: 
+    case 9:
+    return "High";
+  }
+  
+    return answer;
+  }
+  
+  sequentialSizes(1);
+
+//IV
+// 
