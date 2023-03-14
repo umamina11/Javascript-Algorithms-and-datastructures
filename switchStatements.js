90d87031f40cfe379e331e568d891c187bfdb5a2
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-03-14 09:08:48 +0530

    Converting If- Else to switch case

--- switchStatements.js
+++ switchStatements.js
@@ -93,4 +93,30 @@ function sequentialSizes(val) {
   sequentialSizes(1);
 
 //IV
-// 
+// replacing the if- Else statements with switch 
+/** Change the chained if/else if statements into a switch statement.
+*/
+function chainToSwitch(val) {
+    let answer = "";
+  switch(val){
+    case "bob":
+    answer="Marley";
+    break;
+    case 42:
+    answer= "The Answer";
+    break;
+    case 1:
+    answer = "There is no #1";
+    break;
+    case 99:
+    answer = "Missed me by this much!";
+    break;
+    case 7:
+  answer = "Ate Nine";
+  break;
+  
+  }
+    return answer;
+  }
+  
+  chainToSwitch(7);
