5c5325aecc272be3b9f219697565cca4fde20257
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-03-16 22:42:10 +0530

    nested objects

--- objects2.js
+++ objects2.js
@@ -42,4 +42,49 @@ const myMusic = [
     
     }
   ];
+
+
+  //III 
+  //Accessing Nested Objects
+  /** Access the myStorage object and assign the contents of the glove box property to the gloveBoxContents variable. Use dot notation for all properties where possible, otherwise use bracket notation.*/
+  const myStorage = {
+    "car": {
+      "inside": {
+        "glove box": "maps",
+        "passenger seat": "crumbs"
+       },
+      "outside": {
+        "trunk": "jack"
+      }
+    }
+  };
   
+  const gloveBoxContents = myStorage.car.inside["glove box"];
+
+ //IV
+// Accessing nested Arrays
+/**Using dot and bracket notation, set the variable secondTree to the second item in the trees list from the myPlants object. */
+const myPlants = [
+    {
+      type: "flowers",
+      list: [
+        "rose",
+        "tulip",
+        "dandelion"
+      ]
+    },
+    {
+      type: "trees",
+      list: [
+        "fir",
+        "pine",
+        "birch"
+      ]
+    }
+  ];
+  
+  const secondTree = myPlants[1].list[1];
+
+  //V
+  //Record collection
+  //
