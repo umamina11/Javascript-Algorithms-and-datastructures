5ea7fea4f37d3e5cc26b25b911175dfd3d7937af
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-03-17 21:50:49 +0530

    nested for loop

--- whileloops.js
+++ whileloops.js
@@ -53,3 +53,21 @@ let total = 0;
 for (let i = 0; i < myArr.length; i++) {
   total += myArr[i];
 }
+
+//VI 
+//Nesting for loops
+//Modify function multiplyAll so that it returns the product of all the numbers in the sub-arrays of arr.
+
+function multiplyAll(arr) {
+    let product = 1;
+    
+    for (var i = 0; i < arr.length; i++) {
+      for (var j = 0; j < arr[i].length; j++) {
+        product = product * arr[i][j];
+      }
+    }
+
+    return product;
+  }
+  
+  multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
