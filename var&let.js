237aa7373255af808f294c17387324a1fcfca430
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-03-20 10:22:48 +0530

    Arrow functions

--- var&let.js
+++ var&let.js
@@ -29,3 +29,70 @@ function editInPlace() {
 
 }
 editInPlace();
+
+//III
+// Prevent Object Mutation
+/**In this challenge you are going to use Object.freeze to prevent mathematical constants from changing. You need to freeze the MATH_CONSTANTS object so that no one is able to alter the value of PI, add, or delete properties.
+*/
+function freezeObj() {
+  const MATH_CONSTANTS = {
+    PI: 3.14
+  };
+
+  Object.freeze(MATH_CONSTANTS);
+
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch(ex) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
+
+//IV 
+//Use Arrow Functions to Write Concise Anonymous Functions
+
+/**Rewrite the function assigned to the variable magic which returns a new Date() to use arrow function syntax. Also, make sure nothing is defined using the keyword var. */
+const magic = () => {
+  return new Date();
+};
+
+//V
+//Write Arrow Functions with Parameters
+//Rewrite the myConcat function which appends contents of arr2 to arr1 so that the function uses arrow function syntax.
+
+const myConcat=(arr1, arr2) => arr1.concat(arr2);
+
+console.log(myConcat([1, 2], [3, 4, 5]));
+
+
+//VI
+//Set Default Parameters for Your Functions
+//Modify the function increment by adding default parameters so that it will add 1 to number if value is not specified.
+
+const increment = (number, value=1) => number + value;
+
+//VII
+//Use the Rest Parameter with Function Parameters
+//Modify the function sum using the rest parameter in such a way that the function sum is able to take any number of arguments and return their sum.
+
+const sum = (...args) => {
+  let total = 0;
+  for (let i = 0; i < args.length; i++) {
+    total += args[i];
+  }
+  return total;
+}
+console.log(sum(1, 2, 3));
+
+//VIII
+//Use the Spread Operator to Evaluate Arrays In-Place
+//Copy all contents of arr1 into another array arr2 using the spread operator.
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 = [...arr1]; 
+
+console.log(arr2);
