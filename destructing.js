bed9479a29aea2894818c747d39f08f5e5c9eadb
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-03-20 12:35:07 +0530

    destructuring assignment

---
+++ destructing.js
@@ -0,0 +1,10 @@
+//I
+//Use destructuring assignement to Extract value from objects
+/** Replace the two assignments with an equivalent destructuring assignment. It should still assign the variables today and tomorrow the values of today and tomorrow from the HIGH_TEMPERATURES object.*/
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+  };
+
+  const {today, tomorrow} = HIGH_TEMPERATURES;
