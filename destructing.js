5911fb9ca4a9a72c6dcc9b32f03714faa467e823
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-03-21 17:12:33 +0530

    Update destructing.js

--- destructing.js
+++ destructing.js
@@ -23,3 +23,25 @@ const HIGH_TEMPERATURES1 = {
 
     
   const { today: highToday, tomorrow: highTomorrow } = HIGH_TEMPERATURES1;
+
+
+  //III
+  //Use Destructuring Assignment to Assign Variables from Nested Objects
+  /**Replace the two assignments with an equivalent destructuring assignment. It should still assign the variables lowToday and highToday the values of today.low and today.high from the LOCAL_FORECAST object. */
+  const LOCAL_FORECAST = {
+    yesterday: { low: 61, high: 75 },
+    today: { low: 64, high: 77 },
+    tomorrow: { low: 68, high: 80 }
+  };
+  
+
+    
+  const { today: { low: lowToday, high: highToday1 } } = LOCAL_FORECAST;
+  
+
+  //IV
+  //Use Destructuring Assignment to Assign Variables from Arrays
+  /**Use destructuring assignment to swap the values of a and b so that a receives the value stored in b, and b receives the value stored in a. */
+  let a = 8, b = 6;
+
+[a, b] = [b, a];
