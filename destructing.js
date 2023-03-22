d6c95a43ef8b71f6f95708c35149e53edd3e02a0
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-03-22 12:28:52 +0530

    Update destructing.js

--- destructing.js
+++ destructing.js
@@ -45,3 +45,33 @@ const HIGH_TEMPERATURES1 = {
   let a = 8, b = 6;
 
 [a, b] = [b, a];
+
+//V
+//Destructuring via rest elements
+/**Use a destructuring assignment with the rest syntax to emulate the behavior of Array.prototype.slice(). removeFirstTwo() should return a sub-array of the original array list with the first two elements omitted. */
+function removeFirstTwo(list) {
+
+ 
+  const [a, b, ...shorterList] = list;  
+  
+  return shorterList;
+}
+
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const sourceWithoutFirstTwo = removeFirstTwo(source);
+
+//VI
+// Use Destructuring Assignment to Pass an Object as a Function's Parameters
+/**Use destructuring assignment within the argument to the function half to send only max and min inside the function.
+ */
+
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+
+const half = ({ max, min }) => (max + min) / 2.0;
