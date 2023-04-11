66a043a119c8657b54c9021fead04ce53fd3597c
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-04-11 20:14:20 +0530

    Update cases.js

--- algorithms&dataStructures/cases.js
+++ algorithms&dataStructures/cases.js
@@ -29,3 +29,78 @@ Return the resulting array. The input arrays should remain the same after the fu
 function frankenSplice(arr1, arr2, n) {
     return [...arr2.slice(0, n), ...arr1, ...arr2.slice(n)];
   }
+
+//III
+//Falsy bouncer
+/**Falsy Bouncer
+Remove all falsy values from an array. Return a new array; do not mutate the original array.
+
+Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+
+Hint: Try converting each value to a Boolean.
+ */
+
+function bouncer(arr) {
+    return arr.filter(Boolean);
+  }
+
+//IV
+//Where do I Belong
+/**Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number.
+
+For example, getIndexToIns([1,2,3,4], 1.5) should return 1 because it is greater than 1 (index 0), but less than 2 (index 1).
+
+Likewise, getIndexToIns([20,3,5], 19) should return 2 because once the array has been sorted it will look like [3,5,20] and 19 is less than 20 (index 2) and greater than 5 (index 1). */
+
+function getIndexToIns(arr, num) {
+    arr.sort((a, b) => a - b);
+  
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] >= num) return i;
+    }
+  
+    return arr.length;
+  }
+
+  //V
+//Mutataion
+/**
+ * Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+
+For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
+
+The arguments ["hello", "hey"] should return false because the string hello does not contain a y.
+
+Lastly, ["Alien", "line"], should return true because all of the letters in line are present in Alien.
+ */
+
+function mutation(arr) {
+    const test = arr[1].toLowerCase();
+    const target = arr[0].toLowerCase();
+    for (let i = 0; i < test.length; i++) {
+      if (target.indexOf(test[i]) < 0) return false;
+    }
+    return true;
+  }
+  
+
+//VI
+//chunky Monkey
+//Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
+
+function chunkArrayInGroups(arr, size) {
+    let temp = [];
+    const result = [];
+  
+    for (let a = 0; a < arr.length; a++) {
+      if (a % size !== size - 1) temp.push(arr[a]);
+      else {
+        temp.push(arr[a]);
+        result.push(temp);
+        temp = [];
+      }
+    }
+  
+    if (temp.length !== 0) result.push(temp);
+    return result;
+  }
