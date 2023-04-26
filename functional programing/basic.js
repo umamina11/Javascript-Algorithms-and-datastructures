6f84f3169c08f4b3ed22f2577d3097a7960ab763
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-04-26 22:22:26 +0530

    Create basic.js

---
+++ functional programing/basic.js
@@ -0,0 +1,72 @@
+//I
+//Learn About Functional Programming
+//The members of freeCodeCamp happen to love tea.
+
+//In the code editor, the prepareTea and getTea functions are already defined for you. Call the getTea function to get 40 cups of tea for the team, and store them in the tea4TeamFCC variable.
+
+/**
+ * A long process to prepare tea.
+ * @return {string} A cup of tea.
+ **/
+const prepareTea = () => "greenTea";
+
+/**
+ * Get given number of cups of tea.
+ * @param {number} numOfCups Number of required cups of tea.
+ * @return {Array<string>} Given amount of tea cups.
+ **/
+const getTea = numOfCups => {
+  const teaCups = [];
+
+  for (let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+
+  return teaCups;
+};
+
+
+
+const tea4TeamFCC = getTea(40); 
+
+console.log(tea4TeamFCC);
+
+
+
+//II
+//Understand Functional Programming Terminology
+//Prepare 27 cups of green tea and 13 cups of black tea and store them in tea4GreenTeamFCC and tea4BlackTeamFCC variables, respectively. Note that the getTea function has been modified so it now takes a function as the first argument.
+
+//Note: The data (the number of cups of tea) is supplied as the last argument. We'll discuss this more in later lessons.
+
+// Function that returns a string representing a cup of green tea
+const prepareGreenTea = () => 'greenTea';
+
+// Function that returns a string representing a cup of black tea
+const prepareBlackTea = () => 'blackTea';
+
+/*
+Given a function (representing the tea type) and number of cups needed, the
+following function returns an array of strings (each representing a cup of
+a specific type of tea).
+*/
+const getTea1 = (prepareTea, numOfCups) => {
+  const teaCups = [];
+
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+
+// Only change code below this line
+const tea4GreenTeamFCC = getTea1(prepareGreenTea, 27); // :)
+const tea4BlackTeamFCC = getTea1(prepareBlackTea, 13);
+// Only change code above this line
+
+console.log(
+  tea4GreenTeamFCC,
+  tea4BlackTeamFCC
+);
