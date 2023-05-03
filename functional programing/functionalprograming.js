8aeaabfb963613c3d06226275ba52ced1a1789d3
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-05-03 11:00:20 +0530

    functional programming and map method

--- functional programing/functionalprograming.js
+++ functional programing/functionalprograming.js
@@ -5,10 +5,10 @@
 var fixedValue = 4;
 
 function incrementer() {
-  // Add your code below this line
+  
   return fixedValue + 1;
 
-  // Add your code above this line
+  
 }
 
 var newValue = incrementer(); // Should equal 5
