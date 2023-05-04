3f1271ddfb86c2940d34bba7aa4df0b6d63febf4
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-05-04 20:00:04 +0530

    Update slicemethod.js

---
+++ functional programing/slicemethod.js
@@ -0,0 +1,10 @@
+//Return Part of an Array Using the slice Method
+//Use the slice method in the sliceArray function to return part of the anim array given the provided beginSlice and endSlice indices. The function should return an array.
+
+function sliceArray(anim, beginSlice, endSlice) {
+
+    return anim.slice(beginSlice, endSlice);
+   
+  }
+  var inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
+  sliceArray(inputAnim, 1, 3);
