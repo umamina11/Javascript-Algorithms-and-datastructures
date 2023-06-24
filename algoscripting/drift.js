2c42be44ef0ea249463c48f97f3f606f64fa409c
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-06-24 10:44:03 +0530

    diff- compare two arrays , seek and destroy

---
+++ algoscripting/drift.js
@@ -0,0 +1,10 @@
+//Diff two arrays
+
+//Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays.
+function diffArray(arr1, arr2) {
+    return [...diff(arr1, arr2), ...diff(arr2, arr1)];
+  
+    function diff(a, b) {
+      return a.filter(item => b.indexOf(item) === -1);
+    }
+  }
---
+++ algoscripting/seek&destroy.js
@@ -0,0 +1,19 @@
+//Seek and Destroy
+//You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
+function destroyer(arr) {
+    const valsToRemove = Object.values(arguments).slice(1);
+    const filteredArray = [];
+  
+    for (let i = 0; i < arr.length; i++) {
+      let removeElement = false;
+      for (let j = 0; j < valsToRemove.length; j++) {
+        if (arr[i] === valsToRemove[j]) {
+          removeElement = true;
+        }
+      }
+      if (!removeElement) {
+        filteredArray.push(arr[i]);
+      }
+    }
+    return filteredArray;
+  }
