96b6183af05745298252a120e6310d3350fae151
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-06-24 10:45:25 +0530

    Create wherefore.js

---
+++ algoscripting/wherefore.js
@@ -0,0 +1,14 @@
+//Wherefore art thou
+//Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching name and value pairs (second argument). Each name and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
+
+function whatIsInAName(collection, source) {
+    // "What's in a name? that which we call a rose
+    // By any other name would smell as sweet.”
+    // -- by William Shakespeare, Romeo and Juliet
+    const souceKeys = Object.keys(source);
+  
+    // filter the collection
+    return collection.filter(obj => souceKeys
+        .map(key => obj.hasOwnProperty(key) && obj[key] === source[key])
+        .reduce((a, b) => a && b));
+  }
